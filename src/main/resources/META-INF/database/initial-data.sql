8a8a6d8fe87ec95ea2da9a372be97843fde7ab4e
kattyolv <kattyolv@gmail.com>
2021-02-01 15:44:39 +0000

    Add insert sql on client table.

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -1,1 +1,5 @@
 INSERT INTO product (id, name, description, price) VALUES (1, 'Kindle', 'The best kindle ever.', 199.00);
+INSERT INTO product (id, name, description, price) VALUES (2, 'Smartphone', 'The best one.', 99.00);
+
+INSERT INTO client (id, name) VALUES (1, 'Amelia');
+INSERT INTO client (id, name) VALUES (2, 'Alyssa');
