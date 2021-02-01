1f93b1bdd90bcdfdb6655931e6d078f051511286
kattyolv <kattyolv@gmail.com>
2021-02-01 18:03:09 +0000

    Includes slip payment entity.

---
+++ src/main/java/com/algaworks/jpa/ecommerce/model/SlipPayment.java
@@ -0,0 +1,34 @@
+package com.algaworks.jpa.ecommerce.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "slip_payment")
+@Entity
+public class SlipPayment {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Integer id;
+
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPayment status;
+
+    @Column(name = "bar_code")
+    private String barCode;
+
+}
