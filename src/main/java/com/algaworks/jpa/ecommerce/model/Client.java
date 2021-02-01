54b5c7dc74969730d112f0855d6e8efb7e223fe3
kattyolv <kattyolv@gmail.com>
2021-02-01 15:43:52 +0000

    Includes client entity.

---
+++ src/main/java/com/algaworks/jpa/ecommerce/model/Client.java
@@ -0,0 +1,23 @@
+package com.algaworks.jpa.ecommerce.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "client")
+@Entity
+public class Client {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Integer id;
+    private String name;
+
+}
