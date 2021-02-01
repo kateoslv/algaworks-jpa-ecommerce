8de39dd582d3f63ada4ca2c228e5859f8c743387
kattyolv <kattyolv@gmail.com>
2021-02-01 17:59:40 +0000

    Includes order entity.

---
+++ src/main/java/com/algaworks/jpa/ecommerce/model/Order.java
@@ -0,0 +1,45 @@
+package com.algaworks.jpa.ecommerce.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "ordering")
+@Entity
+public class Order {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Integer id;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "conclusion_date")
+    private LocalDateTime conclusionDate;
+
+    @Column(name = "invoice_id")
+    private Integer invoiceId;
+
+    private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    private StatusOrder status;
+
+    @Embedded
+    private OrderDeliveryAddress deliveryAddress;
+
+}
