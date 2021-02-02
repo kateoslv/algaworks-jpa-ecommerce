ee48772af384d672280bf7b6be83fd9858f7ccb7
kattyolv <kattyolv@gmail.com>
2021-02-02 16:16:18 +0000

    Implements auto relationship on category entity.

--- src/main/java/com/algaworks/jpa/ecommerce/model/Category.java
+++ src/main/java/com/algaworks/jpa/ecommerce/model/Category.java
@@ -8,7 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Getter
 @Setter
@@ -24,6 +28,11 @@ public class Category {
 
     private String name;
 
-    private Integer category;
+    @ManyToOne
+    @JoinColumn(name = "parent_category")
+    private Category parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory")
+    private List<Category> categories;
 
 }
