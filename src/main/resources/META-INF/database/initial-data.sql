5e75a2dab8c365deb4b80426aaec45ea81069a4a
kattyolv <kattyolv@gmail.com>
2021-02-02 17:08:01 +0000

    Includes insert to category table.

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -3,3 +3,9 @@ INSERT INTO product (id, name, description, price) VALUES (2, 'Smartphone', 'The
 
 INSERT INTO client (id, name) VALUES (1, 'Amelia');
 INSERT INTO client (id, name) VALUES (2, 'Alyssa');
+
+INSERT INTO ordering (id, fk_client, order_date, total, status) VALUES (1, 1, sysdate(), 100.0, 'WAITING');
+
+INSERT INTO order_item (id, fk_order, fk_product, product_price, amount) VALUES (1, 1, 1, 5.0, 2);
+
+INSERT INTO category (id, name) VALUES (1, 'Eletronics');
