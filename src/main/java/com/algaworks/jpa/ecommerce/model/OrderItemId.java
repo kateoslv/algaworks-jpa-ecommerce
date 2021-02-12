14195522cc91e3ed989292bc64f2815ca3c173b0
kattyolv <kattyolv@gmail.com>
2021-02-12 16:08:54 +0000

    Implements embeddable and column annotations.

--- src/main/java/com/algaworks/jpa/ecommerce/model/OrderItemId.java
+++ src/main/java/com/algaworks/jpa/ecommerce/model/OrderItemId.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Getter
@@ -13,12 +15,15 @@ import java.io.Serializable;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class OrderItemId implements Serializable {
 
     @EqualsAndHashCode.Include
+    @Column(name = "fk_order")
     private Integer idOrder;
 
     @EqualsAndHashCode.Include
+    @Column(name = "fk_product")
     private Integer idProduct;
 
 }
