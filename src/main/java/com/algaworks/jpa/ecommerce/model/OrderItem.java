6158e4f344098a5ddcb87ed6fdb465a02032502d
kattyolv <kattyolv@gmail.com>
2021-02-12 15:40:17 +0000

    Removes id attribute and includes id order and id product attributes.

--- src/main/java/com/algaworks/jpa/ecommerce/model/OrderItem.java
+++ src/main/java/com/algaworks/jpa/ecommerce/model/OrderItem.java
@@ -6,9 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -17,21 +16,27 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@IdClass(OrderItemId.class)
 @Table(name = "order_item")
 @Entity
 public class OrderItem {
 
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @Column(name = "fk_order")
+    private Integer idOrder;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "fk_product")
+    private Integer idProduct;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "fk_order")
+    @JoinColumn(name = "fk_order", insertable = false, updatable = false)
     private Order order;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "fk_product")
+    @JoinColumn(name = "fk_product", insertable = false, updatable = false)
     private Product product;
 
     @Column(name = "product_price")
