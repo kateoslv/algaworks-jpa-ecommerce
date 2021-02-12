9ea209ba3fb6113de1a14fec0ee237a17f5c1f77
kattyolv <kattyolv@gmail.com>
2021-02-12 15:36:59 +0000

    Includes creation date on product and ordering tables and removes id from order item table.

--- src/main/resources/META-INF/database/initial-data.sql
+++ src/main/resources/META-INF/database/initial-data.sql
@@ -1,11 +1,11 @@
-INSERT INTO product (id, name, description, price) VALUES (1, 'Kindle', 'The best kindle ever.', 199.00);
-INSERT INTO product (id, name, description, price) VALUES (2, 'Smartphone', 'The best one.', 99.00);
+INSERT INTO product (id, creation_date, name, description, price) VALUES (1, date_sub(sysdate(), interval  1 day), 'Kindle', 'The best kindle ever.', 199.00);
+INSERT INTO product (id, creation_date,  name, description, price) VALUES (2, date_sub(sysdate(), interval  1 day), 'Smartphone', 'The best one.', 99.00);
 
 INSERT INTO client (id, name) VALUES (1, 'Amelia');
 INSERT INTO client (id, name) VALUES (2, 'Alyssa');
 
-INSERT INTO ordering (id, fk_client, order_date, total, status) VALUES (1, 1, sysdate(), 100.0, 'WAITING');
+INSERT INTO ordering (id, fk_client, creation_date, total, status) VALUES (1, 1, sysdate(), 100.0, 'WAITING');
 
-INSERT INTO order_item (id, fk_order, fk_product, product_price, amount) VALUES (1, 1, 1, 5.0, 2);
+INSERT INTO order_item (fk_order, fk_product, product_price, amount) VALUES (1, 1, 5.0, 2);
 
 INSERT INTO category (id, name) VALUES (1, 'Eletronics');
