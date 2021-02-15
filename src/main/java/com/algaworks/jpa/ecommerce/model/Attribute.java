cfa32f038a0b8fc63a9d11563d5caf7d20aaaca9
kattyolv <kattyolv@gmail.com>
2021-02-15 15:47:00 +0000

    Includes attribute model.

---
+++ src/main/java/com/algaworks/jpa/ecommerce/model/Attribute.java
@@ -0,0 +1,21 @@
+package com.algaworks.jpa.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Attribute {
+
+    private String name;
+
+    private String description;
+
+}
